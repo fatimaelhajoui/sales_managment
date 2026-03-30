209a0cfd4c9747b098405a6bf205589568bbcc27
Fatima Zahrae EL HAJOUI <113965747+fatimaelhajoui@users.noreply.github.com>
2026-03-30 18:06:22 +0100

    Refactor SaleController to manage sales and add functionality

--- sales/sales/src/main/java/net/elhajoui/sales/controllers/SaleController.java
+++ sales/sales/src/main/java/net/elhajoui/sales/controllers/SaleController.java
@@ -4,10 +4,21 @@
  */
 package net.elhajoui.sales.controllers;
 
+import jakarta.validation.Valid;
+import java.io.IOException;
 import net.elhajoui.sales.entities.Sale;
+import net.elhajoui.sales.services.CustomUserDetails;
+import net.elhajoui.sales.services.SaleServiceImp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
@@ -15,9 +26,42 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class SaleController {
+    @Autowired
+    private SaleServiceImp saleServiceImp;
+    
+    @GetMapping("/my_sales")
+    public String sales(Model model,
+                        @RequestParam(name= "keyword", defaultValue = "")String keyword, 
+                        @RequestParam(name= "page", defaultValue = "0")int page, 
+                        @RequestParam(name= "size", defaultValue = "5") int size){
+        
+        CustomUserDetails loggedInUser = (CustomUserDetails) SecurityContextHolder
+        .getContext()
+        .getAuthentication()
+        .getPrincipal();
+        
+        Long userId = loggedInUser.getId();
+    
+        model.addAttribute( "saleslist", saleServiceImp.getSalesByAgent(userId,keyword, page, size).getContent() );
+        model.addAttribute( "totalPages", new int[saleServiceImp.getSalesByAgent(userId,keyword, page, size).getTotalPages()] );
+        model.addAttribute("currentPage", page);
+        model.addAttribute("keyword", keyword);
+        return "sales/sales";
+    }
+    
     @GetMapping("/sales/add_sale")
     public String teamAdd(Model model){
         model.addAttribute("sale", new Sale()); 
         return "sales/add_form";
     }
+    
+    @PostMapping(path = "/sales/save")
+    public String saveTeam(@Valid Sale sale,@RequestParam("file") MultipartFile file,
+                           Authentication auth, BindingResult bindingResult, Model model) throws IOException{
+        if(bindingResult.hasErrors()) return "/sales/add_sale";
+        saleServiceImp.uploadSale(file, sale.getContractId(), auth.getName());
+        model.addAttribute("successMessage", "Sale uploaded successfully!");
+        return "redirect:/my_sales";
+    }
+    
 }

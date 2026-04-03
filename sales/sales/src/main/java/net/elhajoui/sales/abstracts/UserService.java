3ab6b6fda058949c21fe72b73b071176f1edc989
Fatima Zahrae EL HAJOUI <113965747+fatimaelhajoui@users.noreply.github.com>
2026-04-03 16:59:32 +0100

    Refactor UserService methods for clarity and updates

--- sales/sales/src/main/java/net/elhajoui/sales/abstracts/UserService.java
+++ sales/sales/src/main/java/net/elhajoui/sales/abstracts/UserService.java
@@ -14,8 +14,8 @@ import org.springframework.data.domain.Page;
  */
 public interface UserService {
     Page<AppUser> AllUsers(Long userId, String keyword,int page, int size);
-    AppUser createAppUser(AppUser appUser);
+    AppUser createAppUser(Long user_id,AppUser appUser);
     AppUser editAppUser(Long appUser_id);
-    AppUser updateAppUser(Long appUser_id,AppUser appUser);
+    AppUser updateAppUser(Long appUser_id,UpdateAppUerDto appUser);
     void deleteAppUser(Long appUser_id);
 }
